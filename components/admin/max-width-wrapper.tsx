38725c1bc38a52198e473e2335332e88b1cbc9de
Satyam Pathak <Satyampathak08122000k@gmail.com>
2025-08-19 15:55:41 +0530

    intial commit

--- components/admin/max-width-wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { cn } from '@/lib/utils'
-import React from 'react'
-
-export default function MaxWidthWrapper({ children, className = '' }: { children: React.ReactNode; className?: string }) {
-	return <div className={cn(' max-w-[94rem]  mx-auto', className)}>{children}</div>
-}
--- components/principal/max-width-wrapper.tsx
+++
@@ -1,6 +0,0 @@
-import { cn } from '@/lib/utils'
-import React from 'react'
-
-export default function MaxWidthWrapper({ children, className = '' }: { children: React.ReactNode; className?: string }) {
-	return <div className={cn('container px-4 mx-auto', className)}>{children}</div>
-}
